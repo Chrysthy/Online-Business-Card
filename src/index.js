7e23d9bfa695d5a0a6bb89f1ca47fcc9d7b22052
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-28 14:23:53 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ function toggleMode() {
 
     const html = document.documentElement
 
-    // if (html.classList.contains('light')) {
-    //     html.classList.remove('light')
-
-    // } else {
-    //     html.classList.add('light')
-    // }
-
-
     html.classList.toggle('light')
+
+
+    //pegar a tag imagem
+    const img = document.querySelector('#profile img')
+
+
+
+    //substituir a imagem
+    if (html.classList.contains('light')) {
+
+        //light mode, adicionar nova imagem
+        img.setAttribute('src', './img/sol.jpg')
+
+    } else {
+        //dark mode, manter imagem 
+
+        img.setAttribute('src', './img/lua-stars.png')
+
+    }
+
+
+
+
 
 }
