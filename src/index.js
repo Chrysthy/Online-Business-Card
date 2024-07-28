e0b3cc431c27a912616aa0be1c146bdde9fb698f
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-28 14:33:07 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -5,19 +5,14 @@ function toggleMode() {
     html.classList.toggle('light')
 
 
-    //pegar a tag imagem
+   
     const img = document.querySelector('#profile img')
 
-
-
-    //substituir a imagem
     if (html.classList.contains('light')) {
 
-        //light mode, adicionar nova imagem
         img.setAttribute('src', './src/img/sol.jpg')
 
     } else {
-        //dark mode, manter imagem 
 
         img.setAttribute('src', './src/img/lua-stars.png')
 
