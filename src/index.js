2bc80d33c384c6b5c3eeb9573c7144e3fb971033
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-28 14:28:34 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ function toggleMode() {
     if (html.classList.contains('light')) {
 
         //light mode, adicionar nova imagem
-        img.setAttribute('src', './img/sol.jpg')
+        img.setAttribute('src', './src/img/sol.jpg')
 
     } else {
         //dark mode, manter imagem 
 
-        img.setAttribute('src', './img/lua-stars.png')
+        img.setAttribute('src', './src/img/lua-stars.png')
 
     }
 
